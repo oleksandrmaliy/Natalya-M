bcbb3d273c71cf4fca8b77f62daa28abeb981f1f
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-03-08 13:46:29 +0200

    initial

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<>
+			<div className='container grid grid-cols-1 gap-16 sm:grid-cols-3 border-4 border-green-500 sm:border-yellow-500 md:border-red-500 lg:border-blue-500 xl:border-orange-500'>
+				<div>
+					<p>
+						Lorem ipsum, dolor sit amet consectetur adipisicing
+						elit. Sunt vero nam voluptatem harum vitae et ex
+						suscipit eos repellendus placeat, aliquid non nulla iure
+						quia ipsam ullam quis, sed fugiat. Libero quibusdam
+						aliquam perspiciatis tenetur ratione, tempora assumenda
+						cupiditate eveniet, corporis officia alias ipsum sint
+						quam nihil expedita sit culpa nam dolores delectus,
+						magnam fugiat facere. Maiores fugit mollitia repellat.
+					</p>
+				</div>
+				<div className='sm:col-span-2'>
+					<p>
+						Lorem ipsum dolor, sit amet consectetur adipisicing
+						elit. Sunt at, cum obcaecati non nulla incidunt quis
+						maiores, expedita exercitationem adipisci corrupti aut
+						hic nesciunt necessitatibus quisquam sit. Cupiditate,
+						adipisci quo! Distinctio expedita maiores itaque veniam,
+						eveniet voluptate illo, nostrum quod at eum molestiae
+						est rerum animi cum officiis voluptas ad eaque dolore
+						consequatur nulla sint! Dolorum eius repellat laboriosam
+						quis! Modi reprehenderit ducimus a pariatur eligendi,
+						quam itaque velit nam incidunt blanditiis placeat
+						dolores in, reiciendis illo sapiente error at ut hic
+						dolore deleniti rerum, libero quo iure. Dolorem,
+						delectus. Eius commodi velit, quae magnam officia sit
+						quo. Aut voluptatum eligendi, recusandae quasi a
+						excepturi quia itaque harum incidunt fugiat hic ad
+						libero nulla nihil, reprehenderit magni odio accusantium
+						beatae.
+					</p>
+				</div>
+			</div>
+			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+
+			<div className='card'>
+				<button onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+			</div>
+		</>
+	);
+}
+
+export default App;
