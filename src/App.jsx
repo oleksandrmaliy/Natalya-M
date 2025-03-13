e5d85749c91972f2fcc76e814cf88dc97e0d83ed
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-03-13 21:13:13 +0200

    helmet added

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Container from './components/container';
 import BgWrapper from './components/bgWrapper.jsx';
 import Header from './components/header.jsx';
 import HeroSet from './components/heroSet.jsx';
-// import BlockHeader from './components/heroSet.jsx';
 import BlockShop from './components/blockShop.jsx';
 import BlockForever from './components/blockForever.jsx';
 import Footer from './components/footer.jsx';
@@ -19,8 +18,29 @@ const App = () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>My Title</title>
-        <link rel="canonical" href="http://mysite.com/example" />
+        <title>Купити продукцію Форевер | Forever Living Products</title>
+        <meta
+          name="description"
+          content="Купити продукцію Форевер від Forever Living Products. Натуральні продукти на основі алое вера для здоров'я, краси та гарного самопочуття."
+        />
+        <meta name="author" content="Natalya M" />
+        <meta
+          name="keywords"
+          content="Купити продукцію Форевер, Forever Living, алое вера, натуральні добавки, здорове харчування, догляд за шкірою, вітаміни, косметика"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Купити продукцію Форевер | Forever Living Products"
+        />
+        <meta
+          property="og:description"
+          content="Замовляйте якісні продукти Forever Living на основі алое вера. Натуральні рішення для краси, здоров'я та гарного самопочуття."
+        />
+        <meta property="og:image" content="URL_до_зображення" />
+        <meta property="og:url" content="https://natalya-m.vercel.app/" />
+        <link rel="canonical" href="https://natalya-m.vercel.app/" />
       </Helmet>
 
       <BgWrapper styles={headerBgStyles}>
