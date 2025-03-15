2ee4abc960499962682d3132a59a8b30f08398bc
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-03-15 22:45:40 +0200

    button titles

--- src/constants/foreverLinks.js
+++ src/constants/foreverLinks.js
@@ -1,14 +1,14 @@
 const foreverLinks = [
   {
-    title: 'Головна сторінка Forever Ukraine',
+    title: 'Головна сторінка Forever',
     url: 'https://foreverliving.ua/uk/',
   },
   {
-    title: 'Приєднатися до Forever Ukraine',
+    title: 'Приєднатися до Forever',
     url: 'https://foreverliving.ua/uk/user/register/select/?ref=380500014529&',
   },
   {
-    title: 'Вікіпедія про Forever Living',
+    title: 'Вікіпедія про Forever',
     url: 'https://uk.wikipedia.org/wiki/Forever_Living_Products',
   },
 ];
