3b3a98529c6d249db994036fe226a5d95eda7885
MaliyCo <115729512+oleksandrmaliy@users.noreply.github.com>
2025-03-23 22:40:33 +0200

    ogimage

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -4,29 +4,24 @@ const OgImage = () => {
   return (
     <BgWrapper
       styles={
-        'relative bg-[url("/ogimage01.jpeg")] h-[837px] w-[1600px] bg-cover bg-center mb-10'
+        'relative bg-[url("/ogimage.jpg")] h-[837px] w-[1600px] bg-cover bg-center mb-10'
       }
     >
-      <div className="relative z-10">
-        <div className="ml-auto flex w-[780px] flex-col items-center justify-center border-4 border-red-500 pt-[10px]">
-          <h2 className="border-4 border-red-500 pt-[200px] text-center font-poiret text-7xl text-white">
+      {/* <div className="absolute inset-x-0 top-0 h-[50%] bg-gradient-to-b from-blue-900/100 to-white/0"></div> */}
+      {/* <div className="relative z-10">
+        <h1 className="pt-[0px] text-center font-vibes text-[250px] text-white">
+          Пані Наталя
+        </h1>
+        <div className="ml-auto flex w-[870px] flex-col items-center justify-center">
+          <h2 className="pt-[50px] text-center font-poiret text-7xl text-white">
             Продукція компанії
             <br />
             Forever Living Products
           </h2>
 
-          {/* <h2 className="text-center font-poiret text-7xl font-bold text-green-900">
-            <b>
-              Підприємець
-              <br />
-              Форевер
-            </b>
-          </h2> */}
+         
         </div>
-        <h1 className="text-shadow-white-lg border-4 border-red-500 pt-[210px] text-center font-vibes text-[150px] text-white">
-          Пані Наталя
-        </h1>
-      </div>
+      </div> */}
     </BgWrapper>
   );
 };
